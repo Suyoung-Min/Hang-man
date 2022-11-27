d7fac88afad5a0ff2ed3df9fc9dc7f6704848966
unknown <cksdyd8350@gmail.com>
2022-11-28 01:43:37 +0900

    Implemented start button

--- level.js
+++ level.js
@@ -1,6 +1,7 @@
 let easybtn = document.getElementById("easy-btn");
 let mediumbtn = document.getElementById("medium-btn");
 let hardbtn = document.getElementById("hard-btn");
+let startbtn =document.getElementById("start-btn");
 
 let level_description = document.getElementById("level_description");
 
@@ -9,17 +10,18 @@ let level_description = document.getElementById("level_description");
 window.onload = () => {
    
 easybtn.addEventListener("click", e => {
-    level_description.innerText="desciption for easy level";
+    level_description.innerText="description for easy level";
 });
-
 mediumbtn.addEventListener("click", e => {
-    level_description.innerText="desciption for medium level";
+    level_description.innerText="description for medium level";
 
 });
-
 hardbtn.addEventListener("click", e => {
-    level_description.innerText="desciption for hard level";
+    level_description.innerText="description for hard level";
 
+});
+startbtn.addEventListener("click", e => {
+    location.href="index.html";
 });
 
 };
