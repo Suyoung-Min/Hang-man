d6d0b50f5100525e5dd383a3172be3232c57d4df
unknown <cksdyd8350@gmail.com>
2022-11-29 02:16:33 +0900

    Add level desriptions and images

--- level.js
+++ level.js
@@ -10,14 +10,14 @@ let level_description = document.getElementById("level_description");
 window.onload = () => {
    
 easybtn.addEventListener("click", e => {
-    level_description.innerText="description for easy level";
+    level_description_exp.innerText="A1~A2, Beginners level\nRecommended for basic users who can understand sentences and frequently used expressions related to areas of most intermediate areas, such as shopping, family, employment, etc.";
 });
 mediumbtn.addEventListener("click", e => {
-    level_description.innerText="description for medium level";
+    level_description_exp.innerText="B1~B2, Intermediate level\nRecommended for independent users who can understand the main ideas of a complex text on both concrete and abstract topics, including technical discussions in their field of specialization.";
 
 });
 hardbtn.addEventListener("click", e => {
-    level_description.innerText="description for hard level";
+    level_description_exp.innerText="C1~C2, Advanced level\nRecommended for proficient users who can understand a wide range of demanding, longer clauses, and recognize implicit meaning.";
 
 });
 startbtn.addEventListener("click", e => {
